41b1c8d4d11c1f239b1ca97ad461e24c400bbdb3
kbarne12 <kbarne12@gmail.com>
2021-06-11 00:07:39 -0400

    longest string challenge

--- coding_exercise.js
+++ coding_exercise.js
@@ -18,24 +18,44 @@
 //     return false
 // }
 
-const array1 = ['a', 'b', 'c', 'x'];
-const array2 = ['z', 'y', 'x'];
+// const array1 = ['a', 'b', 'c', 'x'];
+// const array2 = ['z', 'y', 'x'];
 
-function containsCommonItem2(arr1, arr2) {
-    let map = {};
-    for (let i=0; i < arr1.length; i++) {
-        if(!map[array1[i]]) {
-            const item = array1[i]
-            map[item] = true;
+// function containsCommonItem2(arr1, arr2) {
+//     let map = {};
+//     for (let i=0; i < arr1.length; i++) {
+//         if(!map[array1[i]]) {
+//             const item = array1[i]
+//             map[item] = true;
+//         }
+//     }
+//     for (let j=0; j < arr2.length; j++) {
+//         if (map[array2[j]]) {
+//             return true;
+//         }
+//     }
+//     return false
+// }
+
+
+// console.log(containsCommonItem2(array1, array2))
+
+// function containsCommonItem3(arr1, arr2) {
+//     return arr1.some(item => arr2.includes(item))
+// }
+
+//  console.log(containsCommonItem3(array1, array2))
+
+listOfString = ['daggie', 'zeke', 'manu', 'kentdrick']
+
+function stringCounter(arr) {
+    let largestString = '';
+    for (let i=0; i<arr.length; i++) {
+        if (arr[i].length > largestString.length) {
+            largestString = arr[i];
         }
     }
-    for (let j=0; j < arr2.length; j++) {
-        if (map[array2[j]]) {
-            return true;
-        }
-    }
-    return false
+    return largestString
 }
 
-
-containsCommonItem2(array1, array2)
+console.log(stringCounter(listOfString))
